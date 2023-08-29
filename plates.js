7e8735ec755c4734894d93b0a474ef7fbab4d5d2
bberci <b.berrci@gmail.com>
2023-08-29 17:41:51 +0200

    listen modification

--- plates.js
+++ plates.js
@@ -81,8 +81,9 @@ mongoose
       });
   });
   
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('Server started on port 3000');
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log('Server started on port lofasz' + PORT);
   });
   
   function generatePlateNumbers() {
